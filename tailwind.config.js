c467f0aee90270a58eaca94de56db3e1117ad05e
moehzi <faizi1234098@gmail.com>
2020-11-25 17:02:31 +0800

    navbar and customizing color tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+		colors:{
+			orange:{
+				DEFAULT:'#F2AD5F',
+				dark:'#986121',
+			}
+		}
+	},
   },
   variants: {
     extend: {},
